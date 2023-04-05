e6d61112f8444e8c7623c700231301a1b30fedae
jokkai2eggs <jokkai2eggs@gmail.com>
2023-04-05 17:42:47 +0700

    2

--- client/app.vue
+++ client/app.vue
@@ -3,3 +3,4 @@
     <NuxtPage></NuxtPage>
   </div>
 </template>
+<script setup lang="ts"></script>
--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -12,11 +12,13 @@ export default defineNuxtConfig({
       script: [
         {
           hid: 'gtmHead',
-          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          innerHTML: `<!-- Google Tag Manager -->
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-K64MD78');`,
+          })(window,document,'script','dataLayer','GTM-K64MD78');</script>
+          <!-- End Google Tag Manager -->`,
         },
       ],
     },
