f5e814a0ff31da9b39ea8201e07dbada6254e022
jokkai2eggs <jokkai2eggs@gmail.com>
2023-04-05 17:32:33 +0700

    test commit

---
+++ client/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen bg-slate-800 text-center py-40">
+    <button
+      id="test ButtonClick"
+      class="text-white text-3xl border-2 border-slate-900 p-2 bg-blue-700 rounded-lg"
+    >
+      Hello Apes!
+    </button>
+  </div>
+</template>
+<script setup lang="ts"></script>
