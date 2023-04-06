8db0075fb0c495e4149b8b7f1dd464e8713ced22
jokkai2eggs <jokkai2eggs@gmail.com>
2023-04-06 13:58:00 +0700

    Update vue-gtag.client.ts

--- client/plugins/vue-gtag.client.ts
+++ client/plugins/vue-gtag.client.ts
@@ -2,8 +2,9 @@ import VueGtag, { trackRouter } from 'vue-gtag-next'
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: 'G-3Z5HQV4NYL',
+      id: 'G-3Z5HQV4NYLZZZZZZZZ',
     },
+    isEnabled: true,
   })
   trackRouter(useRouter())
 })
