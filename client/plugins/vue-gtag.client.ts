646a0ba2ca9aaefc0f463f5e04911f3a7b2f040f
jokkai2eggs <jokkai2eggs@gmail.com>
2023-04-07 10:11:16 +0700

    test no g-tag

--- client/plugins/vue-gtag.client.ts
+++ client/plugins/vue-gtag.client.ts
@@ -2,7 +2,7 @@ import VueGtag, { trackRouter } from 'vue-gtag-next'
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: 'G-3Z5HQV4NYLZZZZZZZZ',
+      id: '',
     },
     isEnabled: true,
   })
