b45e7630c63ab733c13d93114a6e0965fcfd236d
jokkai2eggs <jokkai2eggs@gmail.com>
2023-04-07 10:27:01 +0700

    test add gtm

--- client/plugins/vue-gtag.client.ts
+++ client/plugins/vue-gtag.client.ts
@@ -1,9 +1,12 @@
 import VueGtag, { trackRouter } from 'vue-gtag-next'
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
-    property: {
-      id: 'G-3Z5HQV4NYL',
-    },
+    property: [
+      {
+        id: 'G-3Z5HQV4NYL',
+      },
+      { id: 'GTM-K64MD78' },
+    ],
     isEnabled: true,
   })
   trackRouter(useRouter())
