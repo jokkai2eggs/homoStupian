f93822b3f9bc409f478245499c05d0f637b57336
jokkai2eggs <jokkai2eggs@gmail.com>
2023-04-07 11:36:44 +0700

    Update index.vue

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen bg-slate-800 text-center py-40">
     <button
-      id="test ButtonClick"
+      id="TestButtonClick"
       class="text-white text-3xl border-2 border-slate-900 p-2 bg-blue-700 rounded-lg"
     >
       Hello Apes!
